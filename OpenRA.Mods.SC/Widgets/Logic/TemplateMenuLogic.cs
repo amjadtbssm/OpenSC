8d7a4f5871d06f7b7dd3d48ae0be95e1800add44
Cook Green <dell20091234@gmail.com>
2019-09-13 14:51:46 +0800

    Add Starcraft CD check

--- OpenRA.Mods.SC/Widgets/Logic/TemplateMenuLogic.cs
+++ OpenRA.Mods.SC/Widgets/Logic/TemplateMenuLogic.cs
@@ -9,6 +9,9 @@
  */
 #endregion
 
+using System.IO;
+using System.Linq;
+using System.Text;
 using OpenRA.Mods.Common.Widgets;
 using OpenRA.Widgets;
 
@@ -20,6 +23,59 @@ namespace OpenRA.Mods.Example.Widgets.Logic
 		public TemplateMenuLogic(Widget widget, World world, ModData modData)
 		{
 			widget.Get<ButtonWidget>("QUIT_BUTTON").OnClick = Game.Exit;
+			DetectCD();
 		}
+
+		private void DetectCD()
+        {
+			bool insertedStarcraftCD = false;
+			var drivers = DriveInfo.GetDrives();
+			foreach (var driver in drivers)
+			{
+				if (driver.DriveType == DriveType.CDRom && driver.IsReady)
+				{
+					var di = driver.RootDirectory;
+					var fileFolderList = di.EnumerateFileSystemInfos();
+					var findStarCraftSetupExe = fileFolderList.Where(o => o.Name == "SETUP.EXE");
+					if (findStarCraftSetupExe.Count() == 1)
+					{
+						// Calculate Hash for SETUP.EXE
+						using (FileStream fs = new FileStream(findStarCraftSetupExe.First().FullName, FileMode.Open, FileAccess.Read))
+						{
+							System.Security.Cryptography.SHA1 calculator = System.Security.Cryptography.SHA1.Create();
+							byte[] buffer = calculator.ComputeHash(fs);
+
+							StringBuilder hashBuilder = new StringBuilder();
+							for (int i = 0; i < buffer.Length; i++)
+							{
+								hashBuilder.Append(buffer[i].ToString("x2"));
+							}
+
+							var hash = hashBuilder.ToString();
+							if (hash == "cbb085bd0013f5aa57e5fdec743912ea92e6a9c4")
+							{
+								insertedStarcraftCD = true;
+							}
+						}
+
+						if (insertedStarcraftCD)
+						{
+							break;
+						}
+					}
+				}
+			}
+
+			if (!insertedStarcraftCD)
+			{
+				ConfirmationDialogs.ButtonPrompt(
+					title: "Error",
+					text: "Can't detect the Starcraft CD! Please insert the Starcraft CD to continue.",
+					confirmText: "Retry",
+					cancelText: "Quit",
+					onCancel: Game.Exit,
+					onConfirm: DetectCD);
+			}
+        }
 	}
 }
